f4b414298f2329be5eee7403554880abb3db5e31
TeeronHT <teeron@hajebi.net>
2022-08-21 19:52:27 -0700

    Running through localhost

---
+++ index.js
@@ -0,0 +1,23 @@
+// require the express package in this file
+// and store it in a variable for later use
+const express = require('express');
+
+// create a new instance of `express`
+const app = express();
+
+// Configure application
+app.set('views', __dirname + '/src/views');
+app.set('view engine', 'twig');
+
+// give our app some basic rules about what to
+// do with a web request
+app.get('/', (request, response) => {
+    response.render('index');
+});
+
+app.use('/assets', express.static('src'));
+
+// tell our app what port to listen for requests on
+app.listen(3000, () => {
+    console.log('App is listening on localhost:3000...');
+});
